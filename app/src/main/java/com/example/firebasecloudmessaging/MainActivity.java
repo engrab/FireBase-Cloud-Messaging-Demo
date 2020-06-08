9e54b8b6af936eb24c5d91be63faa98f1023340f
engrab <engrbilawalhussain@gmail.com>
2020-06-08 08:57:51 +0500

    Initial Commit

--- app/src/main/java/com/example/firebasecloudmessaging/MainActivity.java
+++ app/src/main/java/com/example/firebasecloudmessaging/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.firebasecloudmessaging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.textview);
 
-        if (getIntent() != null && getIntent().hasExtra("key1")){
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
 
-            mTextView.setText("");
-            for (String key : getIntent().getExtras().keySet()) {
-                mTextView.append(key+",  Data: "+getIntent().getExtras().getString(key)+"\n");
-            }
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: "+task.getResult().getToken());
+                            mTextView.setText(task.getResult().getToken());
+                        }
+                        else {
+                            Log.d(TAG, "onComplete: "+task.getException());
+                        }
+                    }
+                });
 
-        }
     }
 }
