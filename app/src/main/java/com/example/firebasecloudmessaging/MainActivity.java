937910fb4573bef1613d8f533682452784a79e7d
engrab <engrbilawalhussain@gmail.com>
2020-06-08 23:14:33 +0500

    Initial Commit

--- app/src/main/java/com/example/firebasecloudmessaging/MainActivity.java
+++ app/src/main/java/com/example/firebasecloudmessaging/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     TextView mTextView;
+    Button mSubScribe, mUnSubScribe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.textview);
+        mSubScribe = findViewById(R.id.btn_subscribe);
+        mUnSubScribe = findViewById(R.id.btn_unsubscribe);
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
                     @Override
@@ -45,5 +51,49 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
+
+        mUnSubScribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unSub();
+            }
+        });
+
+        mSubScribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sub();
+            }
+        });
     }
+
+    private void sub() {
+        FirebaseMessaging.getInstance().subscribeToTopic("football-topic")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                        Toast.makeText(MainActivity.this, "Subscribe", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Subscribe error", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void unSub() {
+
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("football-topic")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                        Toast.makeText(MainActivity.this, "Un Subscribe", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Error Un Subscribe", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
 }
